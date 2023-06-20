2fe0c37d62cae937be55b5eab341f3f6bec84ee1
megakuul <megakuul@gmx.ch>
2023-06-20 21:55:12 +0200

    Added Project Routing and Projects List

---
+++ src/lib/projects.list.js
@@ -0,0 +1,61 @@
+/**
+ * This list is representation for the Project List in the Project Routes
+ * 
+ * 
+ * To add a new Project add a folder to the `routes/projects` folder.
+ * Add a `+page.md` file to add content to it and to prerender it a `+page.ts` file,
+ * containing the `export const prerender = true;` option.
+ * 
+ * Finally you can add a global entry to this object that will represent the Project in the Projects Route.
+ */
+export default [
+    {
+        title: "XMemo",
+        subtitle: "An interactive platform to play memory against other players",
+        route: "xmemo",
+        published: "17.06.2023",
+        mainimage: "xmemo.svg"
+    }, {
+        title: "Gorbit",
+        subtitle: "A blazingly fast and easy to use TCP Network Loadbalancer",
+        route: "gorbit",
+        published: "17.06.2023",
+        mainimage: "xmemo.svg"
+    }, {
+        title: "Test",
+        subtitle: "Im a description of the Test product",
+        route: "Test",
+        published: "17.06.2023",
+        mainimage: "xmemo.svg"
+    }, {
+        title: "Test",
+        subtitle: "Im a description of the Test product",
+        route: "xmemo",
+        published: "17.06.2023",
+        mainimage: "xmemo.svg"
+    }, {
+        title: "Test",
+        subtitle: "Im a description of the Test product",
+        route: "xmemo",
+        published: "17.06.2023",
+        mainimage: "xmemo.svg"
+    }, {
+        title: "Test",
+        subtitle: "Im a description of the Test product",
+        route: "xmemo",
+        published: "17.06.2023",
+        mainimage: "xmemo.svg"
+    }, {
+        title: "Test",
+        subtitle: "Im a description of the Test product",
+        route: "xmemo",
+        published: "17.06.2023",
+        mainimage: "xmemo.svg"
+    }, {
+        title: "Test",
+        subtitle: "Im a description of the Test product",
+        route: "xmemo",
+        published: "17.06.2023",
+        mainimage: "xmemo.svg"
+    }
+]
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,27 @@
-<div>Projects</div>
+<script lang="ts">
+    import list from "$lib/projects.list"
+    import Intersector from "$lib/components/Intersector.svelte";
+</script>
+
+<div class="w-full flex flex-col items-center">
+{#each list as item}
+    <Intersector 
+        classAdditional="hero bg-base-200 hover:bg-base-100 hover:shadow-2xl
+            transition-all duration-700 
+            flex flex-row justify-between 
+            h-40 w-5/6 my-5 lg:my-10 p-4 
+            overflow-hidden rounded-2xl"
+
+        classOnDefault="translate-x-10 shadow-none opacity-0"
+        classOnIntersect="translate-x-0 shadow-xl opacity-100"
+        transition="all ease .5s"
+    >
+        <div class="mr-1">
+            <p class="text-xs lg:text-lg py-2">{item.published}</p>
+            <a href="/projects/{item.route}"><h1 class="cursor-pointer link link-hover text-sm lg:text-xl font-bold">{item.title}</h1></a>
+            <p class="text-xs lg:text-lg py-2">{item.subtitle}</p>
+        </div>
+        <img alt="projectimage" src="/images/{item.mainimage}" class="rounded-lg h-2/4 lg:h-3/4" />
+    </Intersector>
+{/each}
+</div>
