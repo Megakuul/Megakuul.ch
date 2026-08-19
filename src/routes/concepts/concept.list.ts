1f19d01e91ad65f83dd40fa15efde031e59d04e2
Megakuul <linus.moser@megakuul.ch>
2026-08-19 23:57:37 +0200

    feat: add image for ai blogpost

--- src/routes/concepts/concept.list.ts
+++ src/routes/concepts/concept.list.ts
@@ -6,6 +6,12 @@ interface Concept {
 }
 
 const concepts: Record<string, Concept> = {
+  'english-is-the-worst-programming-language': {
+    title: 'English is the worst programming language',
+    description: 'AI is not just the next natural "higher abstraction"',
+    published: '12.07.2026',
+    image: 'chatgpt.svg',
+  },
   'auth-standard-matrjoschka': {
     title: 'Auth Standard Matrjoschka',
     description: 'Some auth implementation patterns to use on top of OpenID Connect',
@@ -17,12 +23,6 @@ const concepts: Record<string, Concept> = {
     description:
       "You need an simple, unfair and time-released lock that doesn't require an atomic swap? Use the cooking lock.",
     published: '06.01.2025',
-    image: 'cooking-lock.webp',
-  },
-  'english-is-the-worst-programming-language': {
-    title: 'English is the worst programming language',
-    description: 'AI is not just the next natural "higher abstraction"',
-    published: '12.07.2025',
     image: 'cooking-lock.webp',
   },
 };
